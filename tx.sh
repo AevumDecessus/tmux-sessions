f53aa2c6368861ec5ca1b8d80de261949be629d4
AevumDecessus <aevum@decess.us>
2025-06-06 11:25:14 -0400

    Added tmux script

---
+++ tx.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+SESSION=$1
+
+if [ ! -f ${SCRIPT_DIR}/${SESSION}.sh ]; then
+  echo "${SCRIPT_DIR}/${SESSION}.sh does not exist to load session"
+  exit 1
+else
+  source ${SCRIPT_DIR}/${SESSION}.sh
+fi
+
+start_session()
+{
+  if [[ ${SESSION} == "docker" ]] || [[ ${SESSION} == "lead" ]]; then
+    tmux new-session -d -s "${SESSION}" -n "${WINDOW}" "ssh docker-${WINDOW}"
+  else
+    tmux new-session -d -s "${SESSION}" -n ${WINDOW}
+  fi
+}
+
+add_windows()
+{
+  INDEX=0
+  for WINDOW in "${windows[@]}"
+  do
+    if [[ ${INDEX} == 0 ]]; then
+      start_session
+    else
+      if [[ ${SESSION} == "docker" ]] || [[ ${SESSION} == "lead" ]]; then
+        tmux new-window -n "${WINDOW}" -t "${SESSION}:${INDEX}" "ssh docker-${WINDOW}"
+      else
+        tmux new-window -n "${WINDOW}" -t "${SESSION}:${INDEX}"
+      fi
+    fi
+    INDEX=$((INDEX+1))
+  done
+}
+
+check_session()
+{
+  if ! $(tmux has-session -t ${SESSION}); then
+    add_windows
+  fi
+  attach_session
+}
+
+attach_session()
+{
+  tmux attach-session -t ${SESSION}
+}
+eval "$(ssh-agent -s)"
+ssh-add
+check_session
